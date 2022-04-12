dd35abb0c1cd820efabd36f75a1ba96d51914d0a
Gabriel Pereira <gabrielrobertcorreia@gmail.com>
2022-04-12 16:18:44 -0300

    Add files via upload

--- Entercom Project/js/script.js
+++ Entercom Project/js/script.js
@@ -1,3 +1,9 @@
-<scri>
-	<alert>teste</alert>
-</scri></scri>
+	function openNav() {
+			  document.getElementById("mySidenav").style.width = "250px";
+			  document.getElementById("main").style.marginLeft = "250px";
+			}
+
+			function closeNav() {
+			  document.getElementById("mySidenav").style.width = "0";
+			  document.getElementById("main").style.marginLeft= "0";
+			}
